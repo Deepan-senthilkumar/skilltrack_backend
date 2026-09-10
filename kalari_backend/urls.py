8558fb136cb9d88e56e28d459324a81b9485cdda
Deepan-Senthil <deepan@oceansoftteam.in>
2026-09-10 17:05:58 +0530

    feat: implement curriculum management views with image upload and problem access logic

--- kalari_backend/urls.py
+++ kalari_backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 def health_check(request):
@@ -18,3 +20,6 @@ urlpatterns = [
     path('api/', include('apps.curriculum.urls')),
     path('api/', include('apps.assignments.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
